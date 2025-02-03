2de62dc6b6b84f6c6233103f0ac14c577568a21a
Jeffrey2081 <87138660+Jeffrey2081@users.noreply.github.com>
2025-02-03 23:05:14 +0530

    Themes

--- rofi/powermenu/powermenu.sh
+++ rofi/powermenu/powermenu.sh
@@ -1,70 +1,34 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
+# Rofi power menu with a black-and-white theme
 
-# Available Styles
-# >> Created and tested on : rofi 1.6.0-1
-#
-# column_circle     column_square     column_rounded     column_alt
-# card_circle     card_square     card_rounded     card_alt
-# dock_circle     dock_square     dock_rounded     dock_alt
-# drop_circle     drop_square     drop_rounded     drop_alt
-# full_circle     full_square     full_rounded     full_alt
-# row_circle      row_square      row_rounded      row_alt
-
-theme="drop_square"
-dir="$HOME/.config/rofi/powermenu"
-
-
-uptime=$(uptime -p | sed -e 's/up //g')
-
-rofi_command="rofi -theme $dir/$theme"
+rofi_command="rofi -dmenu -i -p 'Power Menu' -theme ~/.config/rofi/powermenu/powermenu.rasi"
 
 # Options
-shutdown=" "
-reboot=" "
-lock=" "
-suspend=" "
-logout=" "
+shutdown="  Shutdown"
+reboot="  Reboot"
+lock="  Lock"
+suspend="⏾  Suspend"
+logout="  Logout"
 
-# Message
-msg() {
-	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
-}
+# Display the menu
+chosen=$(echo -e "$shutdown\n$reboot\n$lock\n$suspend\n$logout" | $rofi_command)
 
-# Variable passed to rofi
-options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
-
-chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 2)"
+# Execute the selected command
 case $chosen in
-    $shutdown)
-			systemctl poweroff
+    "$shutdown")
+        systemctl poweroff
         ;;
-    $reboot)
-			systemctl reboot
+    "$reboot")
+        systemctl reboot
         ;;
-    $lock)
-		if [[ -f /usr/bin/i3lock ]]; then
-			i3lock
-		elif [[ -f /usr/bin/betterlockscreen ]]; then
-			betterlockscreen -l
-		fi
+    "$lock")
+        betterlockscreen -l || i3lock
         ;;
-    $suspend)
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
+    "$suspend")
+        systemctl suspend
         ;;
-    $logout)
-			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-				i3-msg exit
-			fi
+    "$logout")
+        pkill -KILL -u "$USER"
         ;;
 esac
