c4df7ac788f64dc994b89d022b3579b5c14d8bdc
Jeffrey2081 <87138660+Jeffrey2081@users.noreply.github.com>
2025-02-03 22:32:10 +0530

    Update bspwm2.sh

--- bspwm2.sh
+++ bspwm2.sh
@@ -33,7 +33,7 @@ cd yay
 makepkg -si --noconfirm
 
 #Installing apps
-yay -Syu --noconfirm bspwm lightdm lxappearance sxhkd rofi polybar picom pavucontrol feh wget feh alacritty thunar nitrogen nerd-fonts zsh  xf86-video-intel gedit
+yay -Syu --noconfirm bspwm lightdm lxappearance sxhkd rofi polybar picom pavucontrol feh wget feh alacritty thunar nitrogen nerd-fonts zsh gedit
 
 # Installing fonts
 yay -S papirus-icon-theme ttf-font-awesome --noconfirm
