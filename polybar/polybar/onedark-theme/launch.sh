93491c9f417faeea6756618752551b5fa65c9086
Jeffrey2081 <87138660+Jeffrey2081@users.noreply.github.com>
2025-02-07 19:16:39 +0530

    Update

---
+++ polybar/polybar/onedark-theme/launch.sh
@@ -0,0 +1,5 @@
+
+
+picom --config ~/.config/polybar/onedark-theme/picom/picom.conf &
+
+sh ~/.config/polybar/onedark-theme/polybar/launch.sh &
---
+++ polybar/polybar/onedark-theme/polybar/scripts/update-system.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Launch Alacritty and run yay -Syu to update the system
+alacritty -e yay -Syu --noconfirm
+
