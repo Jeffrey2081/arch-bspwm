d5e15407379f6dd17ee5ac3b17a8459fb50fd7bd
Jeffrey Manuel <87138660+Jeffrey2081@users.noreply.github.com>
2025-03-15 08:39:01 +0530

    Installed reflector

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Update system and install necessary packages
 sudo sed -i 's/^#ParallelDownloads = [0-9]\+/ParallelDownloads = 5/' /etc/pacman.conf
 sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm base-devel xorg xorg-server xorg-xinit unzip xorg-xrandr xorg-xsetroot dbus
+sudo pacman -S --noconfirm base-devel xorg xorg-server reflector rsync curl xorg-xinit unzip xorg-xrandr xorg-xsetroot dbus
 
 # Make directories
 mkdir ~/.config
